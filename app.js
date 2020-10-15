78d80af694f66a5c31a15e4d3080f6539a623654
Robbie Cahill <insprintorob@users.noreply.github.com>
2020-10-15 22:20:04 +1100

    Remove other endpoint

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ const port = 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.post('/webhook-receive', (request, response) => {
-    response.send({
-        payloadSent: request.body,
-        success: true
-    });
-});
-
 app.post('/tweeted', (request, response) => {
     const { body } = request;
     console.log(body.tweet);
