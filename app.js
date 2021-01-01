b838b522268ce497ec20816526be8fc1a730581d
Robbie Cahill <insprintorob@users.noreply.github.com>
2021-01-01 14:23:52 +1100

    Store endpoint in a constant

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const port = 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.get('/webhook-receive', (request, response) => {
+const WEBHOOK_RECEIVE_ENDPOINT = '/webhook-receive';
+
+app.get(WEBHOOK_RECEIVE_ENDPOINT, (request, response) => {
     const { url } = request;
 
     console.log("Received GET webhook request");
     console.log("Full URL:" + url);
 });
 
-app.post('/webhook-receive', (request, response) => {
+app.post(WEBHOOK_RECEIVE_ENDPOINT, (request, response) => {
     const { body } = request;
 
     console.log("Received POST webhook request to /webhook-receive");
@@ -20,7 +22,7 @@ app.post('/webhook-receive', (request, response) => {
     console.log(body);
 });
 
-app.put('/webhook-receive', (request, response) => {
+app.put(WEBHOOK_RECEIVE_ENDPOINT, (request, response) => {
     const { body } = request;
 
     console.log("Received PUT webhook request to /webhook-receive");
@@ -28,7 +30,7 @@ app.put('/webhook-receive', (request, response) => {
     console.log(body);
 });
 
-app.delete('/webhook-receive', (request, response) => {
+app.delete(WEBHOOK_RECEIVE_ENDPOINT, (request, response) => {
     const { url } = request;
 
     console.log("Received DELETE webhook request");
